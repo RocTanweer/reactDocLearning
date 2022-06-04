fc4bcc2460131e7d801a31ed89d8838e8d29c469
Roc Tanweer <mohammadtanweer769@gmail.com>
2022-06-04 08:55:33 +0530

    ADD: added tailwind css compatibility

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
+import "./main.css";
 
-const App = () => <div>hello eworld</div>;
+const App = () => (
+  <div>
+    <h1 className="text-purple-500 font-sans font-semibold text-5xl first-letter:text-[300px]">React from Scratch!!</h1>
+  </div>
+);
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,44 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
+  },
   output: {
-    filename: "bundle.[hash].js",
     path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+      filename: "index.html",
     }),
   ],
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
@@ -22,7 +48,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ["style-loader", "css-loader", "postcss-loader"],
       },
       {
         test: /\.png|svg|jpg|gif$/,
