12ce6b900a1e978d7f6a65836819761e1e30e127
Roc Tanweer <mohammadtanweer769@gmail.com>
2022-11-13 07:08:02 +0530

    removed prev lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,6 @@
 import React from "react";
-import FilterableProductTable from "./FilterableProductTable.jsx";
 import "./main.css";
 
-const PRODUCTS = [
-  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
-];
-
-const App = () => <FilterableProductTable products={PRODUCTS} />;
+const App = () => <h1>Hello world</h1>;
 
 export default App;
