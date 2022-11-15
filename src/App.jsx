9b513be3d549ab71221ccfbadc14727ed844059e
Roc Tanweer <mohammadtanweer769@gmail.com>
2022-11-15 08:52:34 +0530

    obj in state test1

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
 import React from "react";
-import TestCase from "./TestCase.jsx";
+import ScoreBoard from "./ScoreBoard.jsx";
 import "./main.css";
 
 const App = () => (
   <>
-    <TestCase baseState={0} queue={[1, 1, 1]} expected={1} />
-    <hr />
-    <TestCase baseState={0} queue={[increment, increment, increment]} expected={3} />
-    <hr />
-    <TestCase baseState={0} queue={[5, increment]} expected={6} />
-    <hr />
-    <TestCase baseState={0} queue={[5, increment, 42]} expected={42} />s
+    <ScoreBoard />
   </>
 );
-
-function increment(n) {
-  return n + 1;
-}
-
-increment.toString = () => "n => n + 1";
 
 export default App;
