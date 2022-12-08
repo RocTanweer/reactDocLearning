6d6103f30577682528c78fd74d2cafc8541ca7a4
Roc Tanweer <mohammadtanweer769@gmail.com>
2022-12-08 16:53:51 +0530

    preserving & forcing state of nested comp from/to change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,21 @@ import React, { useState } from "react";
 import Chat from "./Chat.jsx";
 import ContactList from "./ContactList.jsx";
 
-export default function Messenger() {
-  const [to, setTo] = useState(contacts[0]);
-  return (
-    <div>
-      <ContactList contacts={contacts} selectedContact={to} onSelect={(contact) => setTo(contact)} />
-      <Chat contact={to} />
-    </div>
-  );
-}
-
 const contacts = [
   { id: 0, name: "Taylor", email: "taylor@mail.com" },
   { id: 1, name: "Alice", email: "alice@mail.com" },
   { id: 2, name: "Bob", email: "bob@mail.com" },
 ];
+
+function App() {
+  const [to, setTo] = useState(contacts[0]);
+
+  return (
+    <div>
+      <ContactList contacts={contacts} selectedContact={to} onSelect={(contact) => setTo(contact)} />
+      <Chat key={to.id} contact={to} />
+    </div>
+  );
+}
+
+export default App;
