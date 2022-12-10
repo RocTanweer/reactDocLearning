a0b1f32b1963f2f2a59c6ffc55673927428c859a
Roc Tanweer <mohammadtanweer769@gmail.com>
2022-12-10 10:16:19 +0530

    cleared codebase for context challenge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,7 @@
 import React from "react";
-import { useReducer } from "./MyReact";
-import Chat from "./Chat.jsx";
-import ContactList from "./ContactList.jsx";
-import { initialState, messengerReducer } from "./messengerReducer";
-import { contacts } from "./contacts";
 
 function App() {
-  const [state, dispatch] = useReducer(messengerReducer, initialState);
-
-  const message = state.messages[state.selectedId];
-
-  const contact = contacts.find((c) => c.id === state.selectedId);
-
-  return (
-    <div>
-      <ContactList contacts={contacts} selectedId={state.selectedId} dispatch={dispatch} />
-      <Chat key={contact.id} message={message} contact={contact} dispatch={dispatch} />
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
--- src/components/contacts.js
+++
@@ -1,5 +0,0 @@
-export const contacts = [
-  { id: 0, name: "Taylor", email: "taylor@mail.com" },
-  { id: 1, name: "Alice", email: "alice@mail.com" },
-  { id: 2, name: "Bob", email: "bob@mail.com" },
-];
